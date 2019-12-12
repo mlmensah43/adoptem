66066dd430a6e7dcdda467b862868b45dc6dcafa
Mariah Mensah <mariahm207@gmail.com>
2019-12-12 10:24:42 -0600

    fixed localhost route

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from "react-sidebar";
 import '../css/Nav.css';
+import {Link} from 'react-router-dom';
 
 
 class Nav extends React.Component {
@@ -16,26 +17,26 @@ class Nav extends React.Component {
   render(){
       return (
         <div className="Nav flex-row">
-            <div><a href="/"><img className="logo" src={require(`../images/${this.props.color}-logo.svg`)} alt="menu-icon"></img></a></div>
+            <div><Link  to="/"><img className="logo" src={require(`../images/${this.props.color}-logo.svg`)} alt="menu-icon"></img></Link></div>
             <div><img className="adoptem" src={require(`../images/${this.props.color}-logo-2.svg`)} alt="adoptem"></img></div>
             <div>
-              <a href="#/favorites"><img className="heart-icon" src={require(`../images/heart-false.png`)} alt="heart"></img></a>
-              {/* <div><Sidebar
-                pullRight={true}
-                sidebar={
-                  <div className='flex-col menu'>
-                    <span className="menu-links">Menu</span>
-                    <a className="menu-links" href='#/pets'>Pets</a>
-                    <a className="menu-links" href='#/about'>About</a>
-                    <a className="menu-links" href='#/contact'>Contact</a>
-                  </div>
-                  
-                }
-                open={this.state.sidebarOpen}
-                onSetOpen={this.onSetSidebarOpen}
-                styles={{ sidebar: { background: "white" }, root: {position: undefined} }}>
-                <button onClick={() => this.onSetSidebarOpen(true)}><img className="menu-icon" src={require(`../images/${this.props.color}-menu-icon.png`)} alt="menu-icon"></img></button>
-              </Sidebar></div> */}
+              <div><Link to="/favorites"><img className="heart-icon" src={require(`../images/heart-false.png`)} alt="heart"></img></Link></div>
+                {/* <div><Sidebar
+                  pullRight={true}
+                  sidebar={
+                    <div className='flex-col menu'>
+                      <span className="menu-links">Menu</span>
+                      <a className="menu-links" href='#/pets'>Pets</a>
+                      <a className="menu-links" href='#/about'>About</a>
+                      <a className="menu-links" href='#/contact'>Contact</a>
+                    </div>
+                    
+                  }
+                  open={this.state.sidebarOpen}
+                  onSetOpen={this.onSetSidebarOpen}
+                  styles={{ sidebar: { background: "white" }, root: {position: undefined} }}>
+                  <button onClick={() => this.onSetSidebarOpen(true)}><img className="menu-icon" src={require(`../images/${this.props.color}-menu-icon.png`)} alt="menu-icon"></img></button>
+                </Sidebar></div> */}
             </div>
         </div>
       );
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/Pet.css';
+import {Link} from 'react-router-dom';
 
 
 class Pet extends React.Component {
@@ -8,14 +9,14 @@ class Pet extends React.Component {
   render(){
     return (
       <div>
-          <a href={`http://localhost:3000/#/${this.props.name}`}>
+          <Link to={`/${this.props.name}`}>
             <div className="container">
                 <img className="pet-image" src={require(`../images/${this.props.name}.jpg`)} alt="pet"></img>
                 <div className="overlay">
                   <div className="text-1">{this.props.name}</div>
                 </div>
-              </div>
-          </a>
+            </div>
+          </Link>
       </div>
       
     );
